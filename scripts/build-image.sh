58ebb6a9a5e9e133abfec4d4161d06045e181a2d
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2025-07-16 00:18:13 -0400

    fix: updates
    Signed-off-by: Andriy Kalashnykov <AndriyKalashnykov@gmail.com>

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -11,8 +11,13 @@ CONTAINER_IMAGE_NAME=flight-path
 
 cd $SCRIPT_PARENT_DIR
 
-# docker buildx create --name builder --driver docker-container --bootstrap
-# docker buildx use builder
+builders="$(docker buildx ls | grep builder)"
+if [[ $builders == "" ]]; then
+        echo "No builder found, creating builder"
+        docker buildx create --use --name builder --driver docker-container --bootstrap
+else
+        echo "Using existing builder"
+fi
 
 docker buildx build                                             \
         --platform linux/amd64,linux/arm64,linux/arm/v7         \
